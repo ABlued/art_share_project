9b9e63884ed3f4a8bcf859d68c6e8cf86fe5467d
ABlued <skwodhkswkd@naver.com>
2020-07-11 20:47:05 +0900

    Updates

---
+++ precache-manifest.a71a54046082b0414b77122d79508c53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ab1457dd164444af213d67ded5593e",
+    "url": "/art_share_project/index.html"
+  },
+  {
+    "revision": "c81e77e4b568c471e9c7",
+    "url": "/art_share_project/static/css/main.057a5d6f.chunk.css"
+  },
+  {
+    "revision": "d32fcb109a65add95590",
+    "url": "/art_share_project/static/js/2.187dd783.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/art_share_project/static/js/2.187dd783.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c81e77e4b568c471e9c7",
+    "url": "/art_share_project/static/js/main.86e4c8f2.chunk.js"
+  },
+  {
+    "revision": "6cf556390ab37e880f6b",
+    "url": "/art_share_project/static/js/runtime-main.dc8fb762.js"
+  }
+]);
